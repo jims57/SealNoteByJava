287cf79c2e10bbd1ae16ffe9075ef44e4f891b8f
jims57 <jims57@163.com>
2021-03-04 15:36:52 +0800

    init

---
+++ seal/src/main/java/net/zcoo/seal/SealApplication.java
@@ -0,0 +1,13 @@
+package net.zcoo.seal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SealApplication.class, args);
+	}
+
+}
