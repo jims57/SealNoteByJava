2cd2800f84872633113e781b9535c9884ea15fc3
jims57 <jims57@163.com>
2021-03-17 10:29:12 +0800

    test sumbit

--- src/main/java/net/zcoo/seal/SpringBootHelloWorldApplication.java
+++ src/main/java/net/zcoo/seal/SpringBootHelloWorldApplication.java
@@ -3,7 +3,7 @@ package net.zcoo.seal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//
 public class SpringBootHelloWorldApplication {
     public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
